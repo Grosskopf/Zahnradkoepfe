4357d7a81cf187c2041b911fa9a8cae603160051
Grosskopf <gerrit@familiegrosskopf.de>
2018-01-27 04:19:08 +0100

    changed tilemaps

--- Scripts/LevelControl.gd
+++ Scripts/LevelControl.gd
@@ -7,10 +7,14 @@ extends Control
 func _ready():
 	if (get_tree().is_network_server()):		
 		#if in the server, get control of player 2 to the other peeer, this function is tree recursive by default
-		get_node("Wände/Player2").set_network_master(get_tree().get_network_connected_peers()[0])
+		get_node("Objekte/Player2").set_network_master(get_tree().get_network_connected_peers()[0])
 	else:
 		#if in the client, give control of player 2 to itself, this function is tree recursive by default
-		get_node("Wände/Player2").set_network_master(get_tree().get_network_unique_id())
+		get_node("Objekte/Player2").set_network_master(get_tree().get_network_unique_id())
+		
+	var player2tex=ImageTexture.new()
+	player2tex.load("Assets/Vorlage/Player2All.png")
+	get_node("Objekte/Player2/Sprite").texture=player2tex
 	
 	pass
 
